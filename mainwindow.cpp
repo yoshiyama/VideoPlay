c7417f71398ce3c3d49649b50268789c55016da2
yoshiyama <yamamoto.ait@gmail.com>
2023-04-15 09:51:16 +0900

    Initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,40 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QVideoWidget>
+#include <QMediaPlayer>
+#include <QFileDialog>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    QAction *openAction = new QAction(tr("&Open"), this);
+//    QAction *openAction = new QAction(this);
+    openAction->setShortcut(QKeySequence::Open);
+    openAction->setStatusTip(tr("Open a file"));
+    connect(openAction, &QAction::triggered, this, &MainWindow::on_actionOpen_triggered);
+    ui->menuFile->addAction(openAction);
+
+    m_videoWidget = new QVideoWidget(this);
+    setCentralWidget(m_videoWidget);
+
+    // メディアプレイヤーの作成
+    m_player = new QMediaPlayer(this);
+    m_player->setVideoOutput(m_videoWidget);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), "", tr("Video Files (*.mp4)"));
+    if (!fileName.isEmpty()) {
+        m_player->setSource(QUrl::fromLocalFile(fileName));
+        m_player->play();
+    }
+}
---
+++ mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFileDialog>
+#include <QVideoWidget>
+#include <QMediaPlayer>
+#include <QUrl>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_actionOpen_triggered();
+
+private:
+    Ui::MainWindow *ui;
+    QVideoWidget *m_videoWidget; // m_videoWidget をクラスのメンバ変数として宣言
+    QMediaPlayer *m_mediaPlayer;
+    QMediaPlayer *m_player;
+};
+
+#endif // MAINWINDOW_H
