ef3506c24e3b4869053e233be75134471e68c9c4
yoshiyama <yamamoto.ait@gmail.com>
2023-04-15 14:44:36 +0900

    first frame display

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,7 +7,11 @@
 #include <QPushButton>
 #include <QVBoxLayout>
 #include <QSettings>
+#include <QTimer>
+//#include <QMediaContent>
 
+
+//ここは超メイン
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -19,6 +23,7 @@ MainWindow::MainWindow(QWidget *parent) :
     openAction->setShortcut(QKeySequence::Open);
     openAction->setStatusTip(tr("Open a file"));
     connect(openAction, &QAction::triggered, this, &MainWindow::on_actionOpen_triggered);
+//    connect(openAction, &QAction::triggered, this, &MainWindow::handleMediaStatusChanged);
     ui->menuFile->addAction(openAction);
 
     // Quit アクションの追加
@@ -44,6 +49,11 @@ MainWindow::MainWindow(QWidget *parent) :
     m_videoWidget = new QVideoWidget(this);
     layout->addWidget(m_videoWidget);
 
+    // "Play" ボタンを作成
+    QPushButton *playButton = new QPushButton("Play", this);
+    playButton->setFixedSize(100, 50);  // 幅100px, 高さ50pxに設定
+    layout->addWidget(playButton);
+
     // "Stop" ボタンを作成
     QPushButton *stopButton = new QPushButton("Stop", this);
     stopButton->setFixedSize(100, 50);  // 幅100px, 高さ50pxに設定
@@ -54,8 +64,23 @@ MainWindow::MainWindow(QWidget *parent) :
     m_player = new QMediaPlayer(this);
     m_player->setVideoOutput(m_videoWidget);
 
-    // "Stop" ボタンの clicked() シグナルをメディアプレイヤーの stop() スロットに接続
+    // "Play" ボタンの clicked() シグナルをメディアプレイヤーの play() スロットに接続
+    connect(playButton, &QPushButton::clicked, m_player, &QMediaPlayer::play);
+
+    // "Stop" ボタンの clicked() シグナルをメディアプレイヤーの pause() スロットに接続
     connect(stopButton, &QPushButton::clicked, m_player, &QMediaPlayer::pause);
+
+    // "Stop" ボタンの clicked() シグナルをメディアプレイヤーの stop() スロットに接続
+//    connect(stopButton, &QPushButton::clicked, m_player, &QMediaPlayer::pause);
+    connect(m_player, &QMediaPlayer::mediaStatusChanged, this, &MainWindow::handleMediaStatusChanged);
+//    connect(m_player, &QMediaPlayer::mediaStatusChanged, this, &MainWindow::on_actionOpen_triggered);
+
+    // タイマーの作成
+//    m_timer = new QTimer(this);
+//    m_timer->setSingleShot(true); // シングルショットモードに設定
+//    connect(m_timer, &QTimer::timeout, this, &MainWindow::handleTimerTimeout);
+
+
 
     // メインウィンドウに QVBoxLayout を設定
     QWidget *centralWidget = new QWidget(this);
@@ -82,11 +107,42 @@ void MainWindow::on_actionOpen_triggered()
 
     QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), lastPath, tr("Video Files (*.mp4)"));
     if (!fileName.isEmpty()) {
-        m_player->setSource(QUrl::fromLocalFile(fileName));
+//        QMediaPlayer mediaContent(QUrl::fromLocalFile(fileName));
+        m_player->setSource(QUrl::fromLocalFile(fileName)); // QMediaContentを使わずにQUrlを直接設定
+
+//        m_player->setMedia(mediaContent);
+
+//        m_player->setSource(QUrl::fromLocalFile(fileName));
+//        m_timer->start(100); // タイマーを 100ms 後に発火させる
+
         m_player->play();
+//        m_player->setSource();
+//        m_player->pause();
+//        m_player->setPosition(0); // 動画の位置を先頭に戻す
 
         QFileInfo fileInfo(fileName);
         settings.setValue("LastPath", fileInfo.absolutePath());
+    }
+}
+void MainWindow::handleTimerTimeout()
+{
+    m_player->pause(); // 一時停止して、1フレーム目を表示
+    m_player->setPosition(0); // 動画の位置を先頭に戻す
+}
+void MainWindow::handleMediaStatusChanged(QMediaPlayer::MediaStatus status)
+{
+    if (status == QMediaPlayer::LoadedMedia) {
+        m_player->play();
+        QCoreApplication::processEvents();
+//    if (status == QMediaPlayer::BufferedMedia) {
+        m_player->pause(); // 一時停止して、1フレーム目を表示
+        m_player->setPosition(0); // 動画の位置を先頭に戻す
+//        QTimer::singleShot(100, this, [this](){
+//                        m_player->pause();
+//                        m_player->setPosition(0);
+//                    });
+//        m_player->pause(); // 一時停止して、1フレーム目を表示
+//        m_player->setPosition(0); // 動画の位置を先頭に戻す
     }
 }
 // 動画の再生をストップ(pause)するスロット
--- mainwindow.h
+++ mainwindow.h
@@ -23,6 +23,9 @@ public:
 
 private slots:
     void on_actionOpen_triggered();
+    void handleMediaStatusChanged(QMediaPlayer::MediaStatus status);
+//    void handleMediaStatusChanged(QMediaPlayer::State state);
+    void handleTimerTimeout();
 //    void stopVideo();
 
 private:
@@ -30,6 +33,7 @@ private:
     QVideoWidget *m_videoWidget; // m_videoWidget をクラスのメンバ変数として宣言
     QMediaPlayer *m_mediaPlayer;
     QMediaPlayer *m_player;
+    QTimer *m_timer;
 };
 
 #endif // MAINWINDOW_H
